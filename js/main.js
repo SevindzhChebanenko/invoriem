f9572811cc0903a524c95125d7fc0f9574cf1f0f
Sevindzh Chebanenko <misschebanenko@gmail.com>
2023-02-08 14:55:51 +0300

    footer

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        dots: false, 
-        margin: 48,
-        loop: true
-    });
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    dots: false,
+    margin: 48,
+    loop: true,
   });
+});
 
-  const videoBtn = document.querySelector('#video-btn'),
-        videoPicture = document.querySelector('.video__picture'),
-        videoWrapper = document.querySelector('.video')
+const videoBtn = document.querySelector("#video-btn"),
+  videoPicture = document.querySelector(".video__picture"),
+  videoWrapper = document.querySelector(".video"),
+  video = document.querySelector("#video-file");
 
-        
-    videoBtn.addEventListener('click', function () {
-        videoPicture.classList.add('none')
-        videoWrapper.classList.remove('video-overlay')
-    })
+videoWrapper.addEventListener("click", function () {
+  if (video.paused) {
+    videoPicture.classList.add("none");
+    videoWrapper.classList.remove("video-overlay");
+    videoBtn.classList.add("none");
+    video.play();
+  } else {
+    videoWrapper.classList.add("video-overlay");
+    videoBtn.classList.remove("none");
+    video.pause();
+  }
+});
